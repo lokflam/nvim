72f18b5e3d1b090257838e94a7629d44992269ce
Marco Lam <marco.lam@crypto.com>
2024-08-24 14:27:53 +0800

    update spell checks

--- lua/plugins/spelunker.lua
+++ lua/plugins/spelunker.lua
@@ -2,9 +2,9 @@ return {
 	"kamykn/spelunker.vim",
 	config = function()
 		vim.g.spelunker_check_type = 2
-		vim.cmd("highlight SpelunkerSpellBad cterm=undercurl ctermfg=NONE gui=undercurl guifg=NONE guisp=gray")
+		vim.cmd("highlight SpelunkerSpellBad cterm=undercurl ctermfg=NONE gui=undercurl guifg=NONE guisp=NONE")
 		vim.cmd(
-			"highlight SpelunkerComplexOrCompoundWord cterm=undercurl ctermfg=NONE gui=undercurl guifg=NONE guisp=gray"
+			"highlight SpelunkerComplexOrCompoundWord cterm=undercurl ctermfg=NONE gui=undercurl guifg=NONE guisp=NONE"
 		)
 	end,
 }
